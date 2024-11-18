d5d1d495b9ce034a3021ad14a02dd8d6ac0ded38
Muala Kabashi <keds30955@KEDS.kv>
2024-11-18 01:02:38 +0100

    first commit

---
+++ app/src/main/java/com/cacttus/android_gr2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.cacttus.android_gr2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
