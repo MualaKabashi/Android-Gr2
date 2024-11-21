0f0063a43fca752c14ad7a5214a387b1ba7dc15f
Muala Kabashi <keds30955@KEDS.kv>
2024-11-21 13:58:07 +0100

    add constrants layout, view binding in activity

--- app/src/main/java/com/cacttus/android_gr2/MainActivity.kt
+++ app/src/main/java/com/cacttus/android_gr2/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.cacttus.android_gr2
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
+import androidx.appcompat.widget.AppCompatButton
 
 
 class MainActivity : ComponentActivity() {
+    private lateinit var editText : EditText
+    private lateinit var button : AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.calculator_layout)
 
+
+        editText = findViewById(R.id.number)
+        button = findViewById(R.id.btn1)
+
+
+        button.setOnClickListener {
+            editText.setText(button.text)
+        }
     }
 }
 
